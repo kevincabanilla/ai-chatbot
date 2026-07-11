b50800481c548aaf9f1a8374d3edfd9a7b1416d3
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-07-11 21:36:44 +0800

    cleanup autogenerated code

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
   server: {
     port: 3000,
     open: false,
