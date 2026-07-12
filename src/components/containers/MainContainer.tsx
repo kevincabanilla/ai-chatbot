b366aa6984b19314a4bf3a42017bfed4f24ddc9b
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-07-12 09:31:13 +0800

    Display random greeting

--- src/components/containers/MainContainer.tsx
+++ src/components/containers/MainContainer.tsx
@@ -1,12 +1,16 @@
 import { AppCard } from "./AppCard";
+import { GREETINGS } from "@/constants/greetings";
+import { Helper } from "@/utils";
 
 export const MainContainer = () => {
+  const greeting = Helper.pickRandom(GREETINGS);
+
   return (
     <main>
       <div className="relative min-h-screen flex justify-center items-center">
         <div className="max-w-3xl">
           <div className="text-center p-5">
-            <h1 className="text-4xl">AI Chatbot</h1>
+            <h1 className="text-3xl">{greeting}</h1>
           </div>
 
         </div>
