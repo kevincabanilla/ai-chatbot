bba9545bc405184b73fd4a1f090e684390abfc7a
Kevin Cabanilla <kevincabanilla.dev@gmail.com>
2026-08-06 14:19:10 +0800

    import geist italic font

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "@fontsource-variable/geist/wght.css";
+import "@fontsource-variable/geist/wght-italic.css";
 import App from "./App.tsx";
 
 const rootElement = document.getElementById("root");
