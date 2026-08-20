c80b963cdab3b5f01cac10f6cbc919f0d8c9a8fd
Kevin Cabanilla <kevincabanilla.dev@gmail.com>
2026-08-20 20:34:14 +0800

    fix RouterProvider not updating after navigating

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,12 @@
-import { useState } from "react";
-import { BrowserRouter, RouterProvider } from "react-router";
+import { RouterProvider } from "react-router";
+import { StoreProvider } from "./providers/StoreProvider";
 import { routes } from "./router/routes";
 import "./App.css";
-import { MEDIA_QUERIES, useMediaQuery } from "./hooks";
-import { StoreProvider } from "./providers/StoreProvider";
-import { TopToolbar, LeftNav } from "./components/views";
-import { SettingsDialog } from "./components/ui/SettingsDialog";
-import { SearchDialog } from "./components/ui/SearchDialog";
-import { AppContext } from "./contexts/AppContext";
 
 const App = () => {
-  const isMobile = useMediaQuery(MEDIA_QUERIES.lg);
-  const [isCollapsed, setIsCollapsed] = useState(isMobile);
-  const [isSetingsOpen, setIsSetingsOpen] = useState(false);
-  const [isSearchOpen, setIsSearchOpen] = useState(false);
-
   return (
     <StoreProvider>
-      <div className="flex">
-        <BrowserRouter>
-          <LeftNav
-            isCollapsed={isCollapsed}
-            isMobile={isMobile}
-            setIsCollapsed={setIsCollapsed}
-            onSearchClicked={() => {
-              setIsSearchOpen(true);
-            }}
-            onSettingsClicked={() => {
-              setIsSetingsOpen(true);
-            }}
-          />
-
-          <SearchDialog
-            open={isSearchOpen}
-            onClose={() => {
-              setIsSearchOpen(false);
-            }}
-          />
-        </BrowserRouter>
-
-        <div className="flex-1">
-          <TopToolbar
-            isVisible={isMobile}
-            onOpenDrawer={(shouldOpen) => {
-              setIsCollapsed(!shouldOpen);
-            }}
-          />
-
-          <AppContext.Provider
-            value={{
-              isMobile,
-              openSettings: () => {
-                setIsSetingsOpen(true);
-              },
-            }}
-          >
-            <RouterProvider router={routes} />
-          </AppContext.Provider>
-
-          <SettingsDialog
-            open={isSetingsOpen}
-            onClose={() => {
-              setIsSetingsOpen(false);
-            }}
-          />
-        </div>
-      </div>
+      <RouterProvider router={routes} />
     </StoreProvider>
   );
 };
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,15 +1,21 @@
-import { lazy } from "react";
+import { createElement, lazy } from "react";
 import { createBrowserRouter, redirect } from "react-router";
+import AppLayout from "@/components/layout/AppLayout";
 
 const MainView = lazy(() => import("@/components/views/MainView"));
 
 export const routes = createBrowserRouter([
   {
-    path: "/",
-    Component: MainView,
-  },
-  {
-    path: "*",
-    loader: () => redirect("/"),
+    element: createElement(AppLayout),
+    children: [
+      {
+        path: "/",
+        Component: MainView,
+      },
+      {
+        path: "*",
+        loader: () => redirect("/"),
+      },
+    ],
   },
 ]);
