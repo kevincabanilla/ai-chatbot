01f592c52aa9090b20fb6750f65ad68c6cbed166
Kevin Cabanilla <kevincabanilla.dev@gmail.com>
2026-08-24 17:40:43 +0800

    add delete conversation button in mobile toolbar

--- src/components/views/TopToolbar.tsx
+++ src/components/views/TopToolbar.tsx
@@ -1,15 +1,21 @@
 import { AnimatePresence, motion } from "motion/react";
-import { Menu } from "lucide-react";
+import { Menu, Trash2 } from "lucide-react";
 import clsx from "clsx";
 import { AppIconButton } from "../buttons/AppIconButton";
+import { useGetQueryParam, useStateManager } from "@/hooks";
 
 export const TopToolbar = ({
   isVisible,
   onOpenDrawer,
+  onDeleteConversation,
 }: {
   isVisible: boolean;
   onOpenDrawer: (shouldOpen: boolean) => void;
+  onDeleteConversation: (cid: string) => void;
 }) => {
+  const currentConversationId = useGetQueryParam("c");
+  const { isConverstationExists } = useStateManager();
+
   return (
     <AnimatePresence>
       {/* Mobile header */}
@@ -41,6 +47,21 @@ export const TopToolbar = ({
           <h1 className="grow text-xl font-bold tracking-tight ml-3">
             {import.meta.env.VITE_APP_TITLE}
           </h1>
+
+          <div className="grow" />
+
+          {currentConversationId &&
+            isConverstationExists(currentConversationId) && (
+              <AppIconButton
+                variant="plain"
+                label="Delete Conversation"
+                icon={Trash2}
+                className="text-accent hover:text-rose-500"
+                onClick={() => {
+                  onDeleteConversation(currentConversationId);
+                }}
+              />
+            )}
         </motion.header>
       )}
     </AnimatePresence>
