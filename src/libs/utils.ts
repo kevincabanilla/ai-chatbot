0836024cae60279220b6b9a54a00bb4dee16c6c0
Kevin Cabanilla <kevincabanilla.dev@gmail.com>
2026-09-15 13:01:57 +0800

    add copyToClipboard function to utils

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -5,3 +5,11 @@ import { clsx, type ClassValue } from "clsx";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const copyToClipboard = async (text: string) => {
+  try {
+    await navigator.clipboard.writeText(text);
+  } catch (error) {
+    console.error("Failed to copy text:", error);
+  }
+};
